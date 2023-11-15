3feadd4228e2f1b9bdfde1d755a2f48080912b2d
Colton Kvapil <kvapic@gmail.com>
2023-11-15 18:10:52 -0500

    Added draft 1 files

---
+++ components/Nav.js
@@ -0,0 +1,10 @@
+document.getElementById('nav')
+        .innerHTML += 
+        `<div class="nav">
+            <ul>
+                <li><a href="/">home</a></li>
+                <li><a href="/portfolio/">portfolio</a></li>
+                <li><a href="/about/">about me</a></li>
+                <li><a href="/contact/">contact</a></li>
+            </ul>
+        </div>`;
